1cfaf9ee3833ae42733ffee09064449ad3922110
piotr-rzepa <56642437+piotr-rzepa@users.noreply.github.com>
2021-04-30 13:29:17 +0200

    encodeURI search

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -3,5 +3,5 @@ const searchBtn = document.querySelector('#button-search');
 searchBtn.addEventListener('click', async (e) => {
   e.preventDefault();
   const search = document.querySelector('#editText-search').value.trim();
-  location.href = `/home/page1?title=${search}`;
+  location.href = `/home/page1?title=${encodeURIComponent(search)}`;
 });
